6974f585134f0a2863d6e6f4932c4acf42de7c97
vislik <vislik2@gmail.com>
2026-03-10 17:40:23 +0300

    Delete src directory

--- src/components/BeeTrail.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="bee-trail" aria-hidden="true">
-    <span class="bee bee-a"></span>
-    <span class="bee bee-b"></span>
-    <span class="bee bee-c"></span>
-  </div>
-</template>
